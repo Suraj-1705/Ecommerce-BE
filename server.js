4e5af1edd88f5d3862d4893ef173497154889559
k suraj <surajsuraj87850@gmail.com>
2024-03-30 12:19:13 +0530

    third commit

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app =  express();
 
-require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 
 
+app.get("/",(req,res)=>{
+    res.json({ message: 'up'})
+})
 
 
 
